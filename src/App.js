86842880198140784454cfca0a4be62be7ba9790
chauhanyash00 <yashc6128@gmail.com>
2024-06-06 17:59:30 +0530

    no commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navbar from './Components/Navbar.js';
-import TextForm from './Components/TextForm.js';
 
 function App() {
   return (
     <>
-    <Navbar title="Chauhan" aboutText="Contact Us"></Navbar>
-    <TextForm heading="Enter Text To Analyse Below"></TextForm>
+    
     </>
   );
 }
