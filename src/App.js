6343f26968df6b894e0e1502eb32b0921a63986f
chauhanyash00 <yashc6128@gmail.com>
2024-06-08 16:48:29 +0530

    add AboutUs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Navbar from './Components/Navbar.js';
-import TextForm from './Components/TextForm.js';
+// import TextForm from './Components/TextForm.js';
+import AboutUs from './Components/AboutUs.js';
 
 function App() {
   return (
     <>
     <Navbar title="Chauhan" aboutText="ContactUS  "></Navbar>
-    <TextForm></TextForm>
+    {/* <TextForm></TextForm> */}
+    <AboutUs heading="About Us"></AboutUs>
     </>
   );
-}
+} 
 
 export default App;
 
